4ef0ed5c99f5c2a4d4838e25a125cf6ee5df79df
MarkoSopta <77636140+MarkoSopta@users.noreply.github.com>
2024-05-24 21:48:27 +0200

    first commit

---
+++ src/main/java/com/marko/BazePostgres/BazePostgresApplication.java
@@ -0,0 +1,13 @@
+package com.marko.BazePostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazePostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazePostgresApplication.class, args);
+	}
+
+}
